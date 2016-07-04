4f43c9ab10d7b4f91d2a3d2b4498b8e57fa86e84
Mark Meritt <mkmeritt93@gmail.com>
2016-07-04 15:16:08 -0400

    MVC Tutorial

---
+++ Vehicles/Vehicles/Car.h
@@ -0,0 +1,18 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Mark Meritt on 2016-07-04.
+//  Copyright © 2016 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+@end
